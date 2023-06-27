af041bbb917a55732ef84b4462d1806ee69d1e3e
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2023-06-27 13:32:34 +0300

    Initial commit

---
+++ src/main/java/com/zolotyh/spring/rest/configuration/MyConfiguration.java
@@ -0,0 +1,46 @@
+package com.zolotyh.spring.rest.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.zolotyh.spring.rest")
+@EnableWebMvc
+public class MyConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.zolotyh.spring.rest.entity");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect"
+                , "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+}
